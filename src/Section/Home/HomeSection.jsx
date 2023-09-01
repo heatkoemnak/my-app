377e94c6777f768ae291bfb6edaa405cbb04deb0
Heat Koemnak <heatkimnak@gmail.com>
2023-09-01 16:26:55 +0700

    committed

--- src/Section/Home/HomeSection.jsx
+++ src/Section/Home/HomeSection.jsx
@@ -14,6 +14,7 @@ import './home.css';
 import Overview from '../Overview/Overview';
 
 export default function HomeSection() {
+
   return (
     <>
       <Navbar />
@@ -40,8 +41,10 @@ export default function HomeSection() {
       <FeaturedSection />
       <AboutSection />
     </>
+    
   );
 }
+
 
 const SearchANDTitle = styled.div`
   width: 100%;
